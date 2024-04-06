681ae6f860b17fc615b7dec0b11125ba39f65a1b
Don <496156+nikolat@users.noreply.github.com>
2024-04-06 18:24:01 +0900

    login with pubkey

--- src/nostr.ts
+++ src/nostr.ts
@@ -1,11 +1,11 @@
-import { NostrEvent, SimplePool, SubCloser, useWebSocketImplementation } from 'nostr-tools';
+import { Filter, NostrEvent, SimplePool, SubCloser, useWebSocketImplementation } from 'nostr-tools';
 useWebSocketImplementation(require('ws'));
 import { sendSSTP } from './sstp';
 const r = String.raw;
 
 export class NostrUtil {
 
-	#pool?: SimplePool;
+	#pool: SimplePool;
 	#sub?: SubCloser;
 	#relays = [
 		'wss://relay-jp.nostr.wirednet.jp',
@@ -13,73 +13,106 @@ export class NostrUtil {
 		'wss://r.kojira.io',
 	];
 
-	getTimeline = () => {
+	constructor () {
 		this.#pool = new SimplePool();
+	};
+
+	getTimeline = (relays: string[], pubkeys: string[]) => {
+		if (relays.length === 0) {
+			relays = this.#relays;
+		}
 		const profiles = new Map<string, NostrEvent>();
-		const getProfile = this.#getProfile;
-		const escape = this.#escape;
-		this.#sub = this.#pool.subscribeMany(
-			this.#relays,
-			[
-				{
-					kinds: [0, 1],
-					since: Math.floor(Date.now() / 1000),
-				},
-			],
-			{
-				async onevent(event) {
-					switch (event.kind) {
-						case 0:
-							profiles.set(event.pubkey, event);
-							break;
-						case 1:
-							if (!profiles.has(event.pubkey)) {
-								profiles.set(event.pubkey, await getProfile(event.pubkey));
-							}
-							let profile;
-							try {
-								profile = JSON.parse(profiles.get(event.pubkey)?.content ?? '{}');
-							} catch (error) {
-								profile = {};
-							}
-							const script = [
-								r`\![set,autoscroll,disable]\0\b[2]\_q`,
-								URL.canParse(profile.picture) ? r`\_b[${profile.picture},centerx,centery,--option=opaque,--option=use_self_alpha,--option=background]` : '',
-								r`\f[bold,true]${escape(profile.display_name ?? '')}\f[bold,default] @${escape(profile.name ?? '')}\n`,
-								r`${escape(event.content).replaceAll('\n', '\\n')}\e`,
-							].join('');
-							sendSSTP(script, 'note', event.content.replaceAll('\n', '\\n'), profile.name ?? '', profile.display_name ?? '', profile.picture ?? '');
-							break;
-						default:
-							break;
+		const filter: Filter = {
+			kinds: [0, 1],
+			since: Math.floor(Date.now() / 1000),
+		};
+		if (pubkeys.length > 0) {
+			filter.authors = pubkeys;
+		}
+		const onevent = async (event: NostrEvent) => {
+			switch (event.kind) {
+				case 0:
+					profiles.set(event.pubkey, event);
+					break;
+				case 1:
+					if (!profiles.has(event.pubkey)) {
+						const profile = await this.#getProfile(event.pubkey, relays);
+						if (profile)
+							profiles.set(event.pubkey, profile);
 					}
-				},
+					let profile;
+					try {
+						profile = JSON.parse(profiles.get(event.pubkey)?.content ?? '{}');
+					} catch (error) {
+						profile = {};
+					}
+					const script = [
+						r`\![set,autoscroll,disable]\0\b[2]\_q`,
+						URL.canParse(profile.picture) ? r`\_b[${profile.picture},centerx,centery,--option=opaque,--option=use_self_alpha,--option=background]` : '',
+						r`\f[bold,true]${this.#escape(profile.display_name ?? '')}\f[bold,default] @${this.#escape(profile.name ?? '')}\n`,
+						r`${this.#escape(event.content).replaceAll('\n', '\\n')}\e`,
+					].join('');
+					sendSSTP(script, 'note', event.content.replaceAll('\n', '\\n'), profile.name ?? '', profile.display_name ?? '', profile.picture ?? '');
+					break;
+				default:
+					break;
+			}
+		};
+		this.#sub = this.#pool.subscribeMany(
+			relays,
+			[filter],
+			{
+				onevent,
 				oneose() {
 				}
 			}
 		);
 	};
 
-	#getProfile = (pubkey: string): Promise<NostrEvent> => {
+	#getProfile = (pubkey: string, relays: string[]): Promise<NostrEvent | undefined> => {
+		const filter: Filter = { kinds: [0], authors: [pubkey] };
+		return this.#fetchLastEvent(filter, relays);
+	};
+
+	getRelayList = async (pubkey: string, relays: string[] = this.#relays): Promise<string[]> => {
+		const filter: Filter = { kinds: [10002], authors: [pubkey] };
+		const event10002 = await this.#fetchLastEvent(filter, relays);
+		if (event10002 === undefined) {
+			return [];
+		}
+		const relaysToRead: string[] = [];
+		for (const tag of event10002.tags.filter(tag => tag.length >= 2 && tag[0] === 'r')) {
+			if ((tag.length === 2 || tag[2] === 'read') && URL.canParse(tag[1])) {
+				relaysToRead.push(tag[1]);
+			}
+		}
+		return relaysToRead;
+	};
+
+	getFollowList = async (pubkey: string, relays: string[] = this.#relays): Promise<string[]> => {
+		const filter: Filter = { kinds: [3], authors: [pubkey] };
+		const event3 = await this.#fetchLastEvent(filter, relays);
+		if (event3 === undefined) {
+			return [];
+		}
+		return event3.tags.filter(tag => tag.length >= 2 && tag[0] === 'p').map(tag => tag[1]);
+	};
+
+	#fetchLastEvent = (filter: Filter, relays: string[]): Promise<NostrEvent | undefined> => {
 		return new Promise((resolve) => {
-			let eventKind0: NostrEvent;
-			const sub = this.#pool?.subscribeMany(
-				this.#relays,
-				[
-					{
-						kinds: [0],
-						authors: [pubkey],
-					},
-				],
+			let res: NostrEvent | undefined;
+			const sub = this.#pool.subscribeMany(
+				relays,
+				[filter],
 				{
 					onevent(event) {
-						if (eventKind0 === undefined || eventKind0.created_at < event.created_at) {
-							eventKind0 = event;
+						if (res === undefined || res.created_at < event.created_at) {
+							res = event;
 						}
 					},
 					oneose() {
 						sub?.close();
-						resolve(eventKind0);
+						resolve(res);
 					}
 				}
 			);
@@ -100,7 +133,7 @@ export class NostrUtil {
 
 	close = () => {
 		this.#sub?.close();
-		this.#pool?.close(this.#relays);
+		this.#pool.close(this.#relays);
 	};
 
 }
--- src/shiori.ts
+++ src/shiori.ts
@@ -1,17 +1,19 @@
 import * as ShioriJK from 'shiorijk';
 import { NostrUtil } from './nostr';
+import { nip19 } from 'nostr-tools';
 const r = String.raw;
 
 class ShioriEngine {
 
 	#nostrutil?: NostrUtil;
+	#pubkey?: string;
 	#pluginVersion = 'nostryu/0.1';
 	
 	load = (dirpath: string) => {};
 
 	unload = () => { process.exit(); };
 
-	request = (request: ShioriJK.Message.Request) => {
+	request = async (request: ShioriJK.Message.Request) => {
 		const response = new ShioriJK.Message.Response();
 		response.status_line.protocol = 'PLUGIN';
 		response.status_line.version = '2.0';
@@ -25,39 +27,89 @@ class ShioriEngine {
 			response.status_line.code = 204;
 			return response;
 		}
-		if (request.request_line.method === 'NOTIFY') {
-			response.status_line.code = 204;
-			return response;
-		}
-		if (request.request_line.method !== 'GET') {
+		if (request.request_line.method === undefined || !['GET', 'NOTIFY'].includes(request.request_line.method)) {
 			response.status_line.code = 400;
 			return response;
 		}
 		const reference = request.headers.references();
 		let script: string | undefined;
 
+		const menu = (pubkey?: string) => {
+			return r`\0\_qnpub:${pubkey ? nip19.npubEncode(pubkey) : 'none'}\n\n`
+			+ r`\![*]\__q[OnNostryu,on]On\__q\n`
+			+ r`\![*]\__q[OnNostryu,off]Off\__q\n\n`
+			+ r`\![*]\__q[OnNostryu,inputnpub]input npub\__q\n\n`
+			+ r`\![*]\__q[Menu_CANCEL]close\__q\e`
+		};
 		switch (request.headers.ID) {
 			case 'version':
 				response.status_line.code = 200;
 				response.headers.set('Value', this.#pluginVersion);
 				return response;
 			case 'OnMenuExec':
-				script = r`\0\_q${this.#pluginVersion}\n\n\![*]\__q[OnNostryu,1]On\__q\n\![*]\__q[OnNostryu,0]Off\__q\e`;
+				script = menu(this.#pubkey);
 				break;
 			case 'OnNostryu':
-				if (reference[0] === '1') {
-					if (this.#nostrutil === undefined) {
-						this.#nostrutil = new NostrUtil();
-						this.#nostrutil.getTimeline();
-					}
+				switch (reference[0]) {
+					case 'on':
+						if (this.#nostrutil === undefined) {
+							const promise: Promise<void> = new Promise(async (resolve) => {
+								this.#nostrutil = new NostrUtil();
+								let relays: string[] = [];
+								let followList: string[] = [];
+								if (this.#pubkey !== undefined) {
+									relays = await this.#nostrutil.getRelayList(this.#pubkey);
+									followList = await this.#nostrutil.getFollowList(this.#pubkey, relays);
+								}
+								this.#nostrutil.getTimeline(relays, followList);
+								resolve();
+							});
+						}
+						break;
+					case 'off':
+						this.#nostrutil?.close();
+						this.#nostrutil = undefined;
+						break;
+					case 'inputnpub':
+						script = r`\![open,inputbox,npub,,npub1...]\e`;
+						break;
+					default:
+						break;
 				}
-				else {
-					this.#nostrutil?.close();
-					this.#nostrutil = undefined;
+				break;
+			case 'OnUserInput':
+				if (reference[0] === 'npub') {
+					if (reference[1] === undefined) {
+						script = r`\0\_qundefined\e`;
+						break;
+					}
+					if (reference[1] === '') {
+						this.#pubkey = undefined;
+						script = menu(this.#pubkey);
+						break;
+					}
+					const npub: string = reference[1];
+					let dr;
+					try {
+						dr = nip19.decode(npub);
+					} catch (error) {
+						script = r`\0\_q${error}\e`;
+						break;
+					}
+					if (dr.type !== 'npub') {
+						script = r`\0\_qnot npub\![open,inputbox,npub,,npub1...]\e`;
+						break;
+					}
+					this.#pubkey = dr.data;
+					script = menu(this.#pubkey);
 				}
 				break;
 			default:
 				break;
+		}
+		if (request.request_line.method === 'NOTIFY') {
+			response.status_line.code = 204;
+			return response;
 		}
 		if (script) {
 			response.status_line.code = 200;
